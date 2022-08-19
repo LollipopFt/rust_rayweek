ef96a84fbabe16f8048d6f56a2e04739b691354b
LollipopFt <lollipopz1426@gmail.com>
2022-08-19 10:58:18 +0800

    1.3 - vec3 class

--- src/tomain.rs
+++ src/tomain.rs
@@ -1,4 +1,11 @@
+use nalgebra::Vector3;
 use std::io::Write;
+pub type Color = Vector3<f32>;
+pub type Vector = Vector3<f32>;
+pub type Point = Vector3<f32>;
+
+mod color;
+use color::writecolor;
 
 pub fn render(
     buffer: &mut [u8],
@@ -8,25 +15,16 @@ pub fn render(
 ) {
     let tonemap: f32 = 255.999;
     for j in 0..img_height {
-        eprint!("\rscanlines remaining: {j}");
+        eprint!("\rscanlines remaining: {}", img_width - j);
         std::io::stderr().flush().ok();
         for i in 0..img_width {
-            let r = i as f32 / (img_width as f32 - 1.);
-            let g = j as f32 / (img_height as f32 - 1.);
-            let b = 0.25f32;
-
-            let ir = (tonemap * r) as u8;
-            let ig = (tonemap * g) as u8;
-            let ib = (tonemap * b) as u8;
-            setpixel(buffer, pitch, i, j, [ir, ig, ib]);
+            let pixel_color = Color::new(
+                i as f32 / (img_width as f32 - 1.),
+                j as f32 / (img_height as f32 - 1.),
+                0.25f32,
+            );
+            writecolor(buffer, pitch, i, j, pixel_color, tonemap);
         }
     }
     eprintln!("\ndone.");
-}
-
-fn setpixel(buffer: &mut [u8], pitch: usize, x: u32, y: u32, color: [u8; 3]) {
-    let offset = y as usize * pitch + x as usize * 3;
-    buffer[offset] = color[0];
-    buffer[offset + 1] = color[1];
-    buffer[offset + 2] = color[2];
 }
